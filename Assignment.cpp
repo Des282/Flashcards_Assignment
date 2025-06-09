318a6552f6b7d88a3d14354f3d14b18ebce2ff4b
Des282 <desmondchew45@gmail.com>
2025-06-10 01:51:16 +0800

    Update Assignment.cpp

--- Assignment.cpp
+++ Assignment.cpp
@@ -4,30 +4,86 @@
 #include <string>
 
 using namespace std;
-
+int num=0;
 class Flashcard
 {
 public:
-    string question;
-    string answer;
-    int score;
+    string q;
+    string a;
+    int s;
 
-    Flashcard(string question, string answer, int score){}   //define each question and answer
+    Flashcard(string question, string answer, int score){
+        q = question;
+        a = answer;
+        s = score;
+    }   //define each question and answer
 
-
+    void showquestion(){
+        cout<<question<<endl;
+    }
+    void showanswer(){
+        cout<<answer<<endl;
+    }
 };
 
 class SystemProcess //manage the whole system process
 {
 public:
-    Flashcard flashcards[max_flashcards]; // Fixed array instead of vector
-    int count;                      // Number of flashcards currently stored
+    Flashcard* f_array = new Flashcards; // Fixed maximum array instead of vector
+    int num;                  // Number of flashcards currently stored
 
-    SyetemManager() : count(0) {}
+    void UpdateScore(){}
+
+    void
 
 };
 
 class SystemManager{  //to run the function which displaying on menu
+private:
+    SystemProgress progress;
+
+public:
+    SystemManager();
+    void addcard(){
+        string ques;
+        string ans;
+        int s;
+        cout<<"Question: "<<endl;
+        cin>>ques;
+        cout<<"\nAnswer: "<<endl;
+        cin>>ans;
+        cout<<"\nScore: "<<endl;
+        cin>>s;
+        f_array[num] = Flashcard(ques, ans, s);
+    }
+
+    void deletecard(){
+        for (int j=0;j<num;j++){
+            cout<<j+1<<". "<<endl;
+            cout<<"Question: "<<f_array[j].showquestion()<<endl;
+            cout<<"\nAnswer: "<<f_array[j].showanswer()<<endl;
+        }
+        cout<<"\nDelete which no.flashcard: "<<endl;
+        int d;
+        cin<<d;
+        for (j=0;j<num;j++){
+            if(j+1 == d){
+                delete f_array[j];
+                cout<<"\nDelete successfully"<<endl;
+                break;
+            }
+        }
+    }
+
+    void reviewcard(){
+        for(int i=0;i<num;i++){
+
+        }
+    }
+
+    void
+
+
 };
 
 
@@ -45,3 +101,4 @@ int main()
 
     return 0;
 }
+
